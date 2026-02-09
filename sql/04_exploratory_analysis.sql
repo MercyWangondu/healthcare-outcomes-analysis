96ceb0d810ca83eb6fe9485fd27e644f565ac270
Wangondu, Mercy Mumbi <132012844+MercyWangondu@users.noreply.github.com>
2026-02-09 19:59:21 +0300

    Update 04_exploratory_analysis.sql

--- sql/04_exploratory_analysis.sql
+++ sql/04_exploratory_analysis.sql
@@ -1,1 +1,11 @@
+SELECT COUNT(*) FROM patients;
+SELECT COUNT(*) FROM encounters;
+SELECT COUNT(*) FROM medications;
+
+SELECT readmitted, COUNT(*)
+FROM encounters
+GROUP BY readmitted;
+
+SELECT AVG(time_in_hospital)
+FROM encounters;
 
