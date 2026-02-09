0c55a6f722b22cc67b0f1324850a1c5ff4a772f1
Wangondu, Mercy Mumbi <132012844+MercyWangondu@users.noreply.github.com>
2026-02-09 19:04:04 +0300

    Update 02_load_data.sql

--- sql/02_load_data.sql
+++ sql/02_load_data.sql
@@ -1,1 +1,17 @@
+INSERT INTO patients (
+    patient_nbr,
+    race,
+    gender,
+    age_group,
+    weight
+)
+SELECT
+    patient_nbr,
+    MAX(race),
+    MAX(gender),
+    MAX(age),
+    MAX(weight)
+FROM staging_diabetes
+GROUP BY patient_nbr;
+
 
